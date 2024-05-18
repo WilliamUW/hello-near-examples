0fe3e9035036a01ab0da00e47d25739a6329bb44
William Wang <bwilliamwang@gmail.com>
2024-05-18 03:13:28 -0400

    Update near.js

--- near.js
+++ near.js
@@ -1,35 +1,43 @@
 const nearAPI = require("near-api-js");
-const { connect } = nearAPI;
+const { connect, Contract, keyStores, KeyPair } = nearAPI;
 
-// creates keyStore from a private key string
-// you can define your key here or use an environment variable
+const PRIVATE_KEY = "ed25519:5rfmAZX62xHZjq1HCRSmEgt6c1i63VzY7k77Riq4Kmb2pVSb9Y7GpPSkYV2TQJX3sN8NCKpBfRXZs2CBbrxA2bze";
+const ACCOUNT_ID = "delirious-whip.testnet";
+const CONTRACT_ID = "delirious-whip.testnet";
 
-const { keyStores, KeyPair } = nearAPI;
 const myKeyStore = new keyStores.InMemoryKeyStore();
-const PRIVATE_KEY = "by8kdJoJHu7uUkKfoaLd2J2Dp1q1TigeWMG123pHdu9UREqPcshCM223kWadm";
-
-// creates a public / private key pair using the provided private key
 const keyPair = KeyPair.fromString(PRIVATE_KEY);
 
-// Wrap your code in an async function
-(async () => {
-  // adds the keyPair you created to keyStore
-  await myKeyStore.setKey("testnet", "example-account.testnet", keyPair);
-
+async function main() {
+  // Adds the keyPair to the keyStore
+  await myKeyStore.setKey("testnet", ACCOUNT_ID, keyPair);
   console.log("Key added to keyStore successfully");
 
+  // Set up the connection configuration
   const connectionConfig = {
     networkId: "testnet",
-    keyStore: myKeyStore, // first create a key store
+    keyStore: myKeyStore,
     nodeUrl: "https://rpc.testnet.near.org",
     walletUrl: "https://testnet.mynearwallet.com/",
     helperUrl: "https://helper.testnet.near.org",
     explorerUrl: "https://testnet.nearblocks.io",
   };
+
+  // Connect to NEAR
   const nearConnection = await connect(connectionConfig);
-  console.log(nearConnection)
-})().catch(console.error);
+  console.log("Connected to NEAR successfully");
 
+  // Get the account object
+  const account = await nearConnection.account(ACCOUNT_ID);
 
+  // Set up the contract object
+  const contract = new Contract(account, CONTRACT_ID, {
+    viewMethods: ["get_artworks"], // Specify view methods
+  });
 
+  // Call the view method
+  const artworks = await contract.get_artworks();
+  console.log("Artworks:", artworks);
+}
 
+main().catch(console.error);
